f6c8c49675fc11e16f0b8193424838bd9edac057
Saprolin <ericoliverobiol@gmail.com>
2017-03-27 13:06:15 +0200

    CharacterInput refactoring

--- Assets/Scripts/Player/CharacterInput.cs
+++ Assets/Scripts/Player/CharacterInput.cs
@@ -27,8 +27,6 @@ public class CharacterInput : MonoBehaviour {
     Weapon weapon;
 
     // Control
-    float _cursorSensitivity = 1f;
-    Vector3 cursor = Vector3.zero;
     [SerializeField]
     Vector3 aim = Vector3.up;
     //Vector3 lastCursor = Vector3.zero;
@@ -62,27 +60,46 @@ public class CharacterInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        GatherInput();
+        PlayerInputs();
         UpdateJumpTarget();
         UpdateRolling();
     }
 
-    void GatherInput()
+    void PlayerInputs()
     {
         // AIMING
-        cursor = Input.mousePosition;
-        cursor.z = Camera.main.transform.position.z;
-        cursor = Camera.main.ScreenToWorldPoint(cursor);
-        cursor.z = transform.position.z;
-        aim = cursor - transform.position;
-        aim.Normalize();
-
+        AssignAim();
         Debug.DrawRay(transform.position, aim, Color.green);
 
         // MOVEMENT
-        
-        //xAxis = 0;
-        //yAxis = 0;
+        AssignMovementAxis();
+        Running();
+
+        // JUMPING
+        Jumping();
+
+        // ATTACKING
+        Attacking();
+    }
+
+    void AssignAim()
+    {
+        Vector3 cursor = Input.mousePosition;
+        cursor.z = Camera.main.transform.position.z;
+        cursor = Camera.main.ScreenToWorldPoint(cursor);
+        cursor.z = transform.position.z;
+        Vector3 newAim = cursor - transform.position;
+        newAim.Normalize();
+
+        /*Vector3 newAim = new Vector3(xAxis, yAxis, 0f).normalized;
+        if (newAim != Vector3.zero)
+            aim = newAim;*/
+
+        aim = newAim;
+    }
+
+    void AssignMovementAxis()
+    {
         if (controlMode == ControlMode.keyboard)
         {
             xAxis = 0;
@@ -98,69 +115,52 @@ public class CharacterInput : MonoBehaviour {
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
                 yAxis--;
         }
-        else if( controlMode == ControlMode.gamepad)
+        else if (controlMode == ControlMode.gamepad)
         {
             xAxis = Input.GetAxis("X");
             yAxis = Input.GetAxis("Y");
         }
+    }
 
-        /*Vector3 newAim = new Vector3(xAxis, yAxis, 0f).normalized;
-        if (newAim != Vector3.zero)
-            aim = newAim;*/
-
-        if(xAxis != 0 && mov2D.Grounded && !mov2D.GetRolling())
+    void Running()
+    {
+        if (xAxis != 0 && mov2D.Grounded && !mov2D.GetRolling())
         {
             //mov2D.SetVelX(RUN_MAX_VEL * xAxis);
             if (Mathf.Abs(mov2D.GetVelocity().x) < RUN_MIN_VEL)
                 mov2D.SetVelX(RUN_MIN_VEL * Mathf.Sign(xAxis));
             //else
-                //mov2D.SetVelX(Mathf.Clamp(mov2D.GetVelocity().x * RUN_ACCEL, -RUN_MAX_VEL, RUN_MAX_VEL));
+            //mov2D.SetVelX(Mathf.Clamp(mov2D.GetVelocity().x * RUN_ACCEL, -RUN_MAX_VEL, RUN_MAX_VEL));
             mov2D.SetAccel(Vector3.right * RUN_ACCEL * Mathf.Sign(xAxis));
             mov2D.SetVelX(Mathf.Clamp(mov2D.GetVelocity().x, -RUN_MAX_VEL, RUN_MAX_VEL));
         }
+    }
 
+    void Jumping()
+    {
+        if (mov2D.Grounded)
+        {
+            jump = false;
+            doubleJump = false;
+        }
 
-        // JUMPING
         if (!doubleJump && (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.X)))
         {
-            Vector3 jumpDir = new Vector3(xAxis, yAxis, 0f);
-            jumpDir.Normalize();
-            if (jumpDir == Vector3.zero)
-                jumpDir = Vector3.up;
+            Vector3 jumpDir = GetJumpDir();
 
-            float angle = Mathf.Asin(jumpDir.y) * Mathf.Rad2Deg;
-            if (angle < JUMP_ROLL_MARGIN)
-                mov2D.SetRollIntent(true);
+            CheckRollIntent(jumpDir.y);
 
-            Vector3 residualVelocity = mov2D.GetVelocity() / 3f;
-            if (jumpDir.y > 0f)
-                residualVelocity.y = 0f;
-            if (jumpDir.x != 0 && Mathf.Sign(jumpDir.x) == Mathf.Sign(residualVelocity.x))
-                residualVelocity.x = 0f;
+            Vector3 residualVelocity = GetResidualVelocity(jumpDir);
 
             if (mov2D.Grounded)
             {
-                jump = true;
-                if(!mov2D.GetRollIntent())
-                    mov2D.SetVel(residualVelocity + (jumpDir + Vector3.up).normalized * JUMP_FORCE);
-                else
-                    mov2D.SetVel(residualVelocity + jumpDir * JUMP_FORCE);
-                /*if (xAxis != 0 && Mathf.Sign(jumpDir.x) == Mathf.Sign(mov2D.GetVelocity().x))
-                    mov2D.SetVelY(JUMP_FORCE);
-                else
-                    mov2D.SetVel(jumpDir * JUMP_FORCE);*/
+                Jump(jumpDir, residualVelocity);
             }
             else if (!doubleJump)
             {
-                doubleJump = true;
-                
-                //mov2D.SetVel(jumpDir * JUMP_FORCE);
-                if (!mov2D.GetRollIntent())
-                    mov2D.SetVel(residualVelocity + (jumpDir + Vector3.up).normalized * JUMP_FORCE);
-                else
-                    mov2D.SetVel(residualVelocity + jumpDir * JUMP_FORCE);
+                DoubleJump(jumpDir, residualVelocity);
             }
-            else if(mov2D.WallLean != 0)
+            else if (mov2D.WallLean != 0)
             {
                 doubleJump = true;
                 //mov2D.SetVel(jumpDir * JUMP_FORCE);
@@ -170,32 +170,58 @@ public class CharacterInput : MonoBehaviour {
                     mov2D.SetVel((jumpDir + Vector3.right * mov2D.WallLean).normalized * JUMP_FORCE);
             }
         }
+    }
+
+    Vector3 GetJumpDir()
+    {
+        Vector3 jumpDir = new Vector3(xAxis, yAxis, 0f);
+        jumpDir.Normalize();
+        if (jumpDir == Vector3.zero)
+            jumpDir = Vector3.up;
+        return jumpDir;
+    }
+
+    void CheckRollIntent(float jumpDirY)
+    {
+        float angle = Mathf.Asin(jumpDirY) * Mathf.Rad2Deg;
+        if (angle < JUMP_ROLL_MARGIN)
+            mov2D.SetRollIntent(true);
         //if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(1) || Input.GetKeyUp(KeyCode.Z))
         //    mov2D.SetRollIntent(false);
+    }
 
-        if (mov2D.Grounded)
-        {
-            jump = false;
-            doubleJump = false;
-        }
+    Vector3 GetResidualVelocity(Vector3 jumpDir)
+    {
+        Vector3 residualVelocity = mov2D.GetVelocity() / 3f;
+        if (jumpDir.y > 0f)
+            residualVelocity.y = 0f;
+        if (jumpDir.x != 0 && Mathf.Sign(jumpDir.x) == Mathf.Sign(residualVelocity.x))
+            residualVelocity.x = 0f;
+        return residualVelocity;
+    }
 
-        // ATTACKING
-        if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.C))
-        {
-            if (attackButtonRelease && weapon.AttackDown(aim) && weapon.IsMelee && weapon is WeaponStraight)
-            {
-                attackButtonRelease = false;
-                mov2D.AddInstantTraslation(aim * ATTACK_DISPLACEMENT);
-                //transform.position += aim * 0.25f;
-            }
-        }
-        if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.C))
-        {
-            attackButtonRelease = true;
-            if (weapon.AttackUp(aim) && weapon.IsMelee && weapon is WeaponCharged) {
-                mov2D.AddInstantTraslation(aim * ATTACK_DISPLACEMENT);    
-            }
-        }
+    void Jump(Vector3 jumpDir, Vector3 residualVelocity)
+    {
+        jump = true;
+        if (!mov2D.GetRollIntent())
+            mov2D.SetVel(residualVelocity + (jumpDir + Vector3.up).normalized * JUMP_FORCE);
+        else
+            mov2D.SetVel(residualVelocity + jumpDir * JUMP_FORCE);
+        /*if (xAxis != 0 && Mathf.Sign(jumpDir.x) == Mathf.Sign(mov2D.GetVelocity().x))
+            mov2D.SetVelY(JUMP_FORCE);
+        else
+            mov2D.SetVel(jumpDir * JUMP_FORCE);*/
+    }
+
+    void DoubleJump(Vector3 jumpDir, Vector3 residualVelocity)
+    {
+        doubleJump = true;
+
+        //mov2D.SetVel(jumpDir * JUMP_FORCE);
+        if (!mov2D.GetRollIntent())
+            mov2D.SetVel(residualVelocity + (jumpDir + Vector3.up).normalized * JUMP_FORCE);
+        else
+            mov2D.SetVel(residualVelocity + jumpDir * JUMP_FORCE);
     }
 
     void UpdateJumpTarget()
@@ -225,6 +251,27 @@ public class CharacterInput : MonoBehaviour {
                 rollTimer = 0f;
                 mov2D.SetRolling(false);
                 transform.eulerAngles = Vector3.zero;
+            }
+        }
+    }
+
+    void Attacking()
+    {
+        if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.C))
+        {
+            if (attackButtonRelease && weapon.AttackDown(aim) && weapon.IsMelee && weapon is WeaponStraight)
+            {
+                attackButtonRelease = false;
+                mov2D.AddInstantTraslation(aim * ATTACK_DISPLACEMENT);
+                //transform.position += aim * 0.25f;
+            }
+        }
+        if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.C))
+        {
+            attackButtonRelease = true;
+            if (weapon.AttackUp(aim) && weapon.IsMelee && weapon is WeaponCharged)
+            {
+                mov2D.AddInstantTraslation(aim * ATTACK_DISPLACEMENT);
             }
         }
     }
